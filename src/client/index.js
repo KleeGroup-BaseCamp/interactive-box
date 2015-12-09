18272fa21956cfa3d7d7adb82ace52a830b3d74b
antoineb19 <antoinebenoit@msn.com>
2015-12-09 12:10:25 +0100

    Ajout écran d'accueil

--- src/client/index.js
+++ src/client/index.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const MyFirstComponent = React.createClass({
-    render() {
-        return (
-            <div>
-                <h1>Hello !</h1>
-                <p>This is a first React component :)</p>
-            </div>
-        );
-    }
+var WelcomeBox = React.createClass({
+  render: function(){
+    return(
+      <div>
+        <h1 className="index-title">Welcome</h1>
+        <div className="div-button">
+          <LinkButton className="col-xs-6 col-md-6 index-button" text="Administrateur" url="admin"/>
+          <LinkButton className="col-xs-6 col-md-6 index-button" text="Utilisateur" url="user"/>
+        </div>
+      </div>
+    );
+  }
 });
 
-ReactDOM.render(<MyFirstComponent/>, document.getElementById('interactive-box'));
+var LinkButton = React.createClass({
+  gotoPage: function(){
+    if(this.props.url)
+      window.location.href='./'+this.props.url;
+  },
+  render: function(){
+    return(
+      <button className={this.props.className} onClick={this.gotoPage}>{this.props.text}</button>
+    );
+  }
+});
+
+ReactDOM.render(
+  <WelcomeBox/>,
+  document.getElementById('interactive-box')
+);
