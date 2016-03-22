0f377fc09b88a2698e56b93199cd67dbe8fee03e
aliceleducq <leducqalice@gmail.com>
2016-03-22 15:13:25 +0100

    fuite mémoire dans le timer dégagée

--- src/client/Utils/CountdownTimer.js
+++ src/client/Utils/CountdownTimer.js
@@ -5,12 +5,23 @@ import Piechart from 'react-chartjs';
 
 var PieChart = require("react-chartjs").Pie;
 
-
+/*var SetIntervalMixin = {
+  componentWillMount: function() {
+    this.intervals = [];
+  },
+  setInterval: function() {
+    this.intervals.push(setInterval.apply(null, arguments));
+  },
+  componentWillUnmount: function() {
+    this.intervals.forEach(clearInterval);
+  }
+};*/
 
 var CountdownTimer = React.createClass({
 
     duree: 10,
     chartOptions:{animation: false},
+//    mixins: [SetIntervalMixin],
    
     
     getInitialState: function() {
@@ -34,14 +45,21 @@ var CountdownTimer = React.createClass({
         this.setState({ secondsRemaining: this.props.duration });
         
         this.interval = setInterval(this.tick, 1000);
+        this.setState({interval: this.interval});
     },
+    componentWillUnmount () {
+    this.interval && clearInterval(this.interval);
+    this.interval = false;
+},
 
 
     tick: function() {
+        if (this.state.secondsRemaining >= 0) {
         this.setState({secondsRemaining: this.state.secondsRemaining - 1});
         var newData = [{value: this.state.secondsRemaining, color:"#F7464A"}, {value: this.duree-this.state.secondsRemaining, color:"#FF0000"}];
+        }
         if (this.state.secondsRemaining <= 0) {
-            clearInterval(this.interval);
+            clearInterval(this.state.interval);
             this.props.timeOut();
         }
         this.setState({data: newData});
